27e2a3acaed8493bb2a883fd9926aae55431d53a
백엔드 개발자 홍윤기 <81303136+yunkihong-dev@users.noreply.github.com>
2025-09-15 16:51:16 +0900

    hi

---
+++ step10-CICD/src/main/java/com/example/demo/Step10CicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step10CicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step10CicdApplication.class, args);
+	}
+
+}
